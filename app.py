adc2230006e7510911dd8cfe5c9624cc13b719e5
Saymon742 <maklabuhin78@gmail.com>
2025-04-06 10:33:20 +0300

    new structure2

--- app.py
+++ app.py
@@ -4,28 +4,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def base():
-    render_template("base.html")
+    return render_template("base.html")
 
 
 @app.route("/about/")
 def about():
-    render_template("about.html")
+    return render_template("about.html")
 
 
 @app.route("/contact/")
 def contact():
-    render_template("contact.html")
+    return render_template("contact.html")
 
 
 @app.route("/menu/")
 def menu():
-    render_template("menu.html")
+    return render_template("menu.html")
 
 
 @app.route("/reviews/")
 def reviews():
-    render_template("rewiews.html")
+    return render_template("rewiews.html")
 
 
-app.run(port=3001, debug=True)
-# не запускається тому що не відкриває якийсь файл main.py(принайині у мене)
+if __name__ == '__main__':
+    app.run(port=3001, debug=True)
