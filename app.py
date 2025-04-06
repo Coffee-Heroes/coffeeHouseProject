90710aebdad8c34bde5ce328f4462c00070a13db
rifemane5328 <zdanevych2011@gmail.com>
2025-04-06 11:34:32 +0300

    Update app.py
    add some aborts

--- app.py
+++ app.py
@@ -4,18 +4,24 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def about():
-    return render_template("about.html")
+def about(file):
+    if file:
+        return render_template("about.html")
+    abort(403)
 
 
 @app.route("/menu/")
-def menu():
-    return render_template("menu.html")
+def menu(file):
+    if file:
+        return render_template("menu.html")
+    abort(403)
 
 
 @app.route("/reviews/")
-def reviews():
-     return render_template("rewiews.html")
+def reviews(file):
+    if file:
+        return render_template("rewiews.html")
+    abort(403)
 
 
 if __name__ == "__main__":
