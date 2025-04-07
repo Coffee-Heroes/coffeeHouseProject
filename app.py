6bc1c6ec5264d83b3e24f6f685e2f8620f1d0c19
rifemane5328 <zdanevych2011@gmail.com>
2025-04-07 16:29:03 +0300

    Update app.py
    corrected aborts

--- app.py
+++ app.py
@@ -4,25 +4,29 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def about(file):
-    if file:
-        return render_template("about.html")
-    abort(403)
+def base():
+    return render_template("base.html")
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
 
 
 @app.route("/menu/")
-def menu(file):
-    if file:
-        return render_template("menu.html")
-    abort(403)
+def menu():
+    return render_template("menu.html")
 
 
 @app.route("/reviews/")
-def reviews(file):
-    if file:
-        return render_template("rewiews.html")
-    abort(403)
+def reviews():
+    return render_template("rewiews.html")
+
+
+@app.errorhandler(404)  # для каждой страницы если не тот url
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 
 if __name__ == "__main__":
-    app.run(port=3001, debug=True)
+    app.run(port=3001, debug=False)
