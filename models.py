41e824698d555b914219b9e0337d0693230c9829
rifemane5328 <zdanevych2011@gmail.com>
2025-05-02 15:15:27 +0300

    Update models.py
    returned models.py

--- models.py
+++ models.py
@@ -20,17 +20,6 @@ class User(db.Model, UserMixin):
     role = db.Column(SqlEnum(RoleEnum, name="role_enum"), default=RoleEnum.USER, nullable=False)
 
 
-class Order(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    product_name = db.Column(db.String(100), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    customer = db.relationship('User', backref=db.backref('orders', lazy=True))
-    delivery_address = db.Column(db.String(200), nullable=True)
-    comment = db.Column(db.String(200), nullable=True)
-    status = db.Column(db.String(50), default='Pending')
-
-
 class Dish(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String, nullable=False)
@@ -38,6 +27,19 @@ class Dish(db.Model):
     description = db.Column(db.String(200))
     price = db.Column(db.String(10), nullable=False)
     image = db.Column(LargeBinary, nullable=False)
+    image_mime = db.Column(db.String(50), nullable=False)
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('dish.id'), nullable=False)
+    product = db.relationship('Dish', backref=db.backref('orders', lazy=True))
+    quantity = db.Column(db.Integer, nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    customer = db.relationship('User', backref=db.backref('orders', lazy=True))
+    delivery_address = db.Column(db.String(200), nullable=False)
+    comment = db.Column(db.String(200))
+    status = db.Column(db.String(50), default='Pending')
     
     
     
